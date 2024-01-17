452bd4a154e607de2b76f82ea0c95057cf92058c
sandeep-khande <sandeepkhande448@gmail.com>
2024-01-17 19:03:10 +0530

    completed useMemo assignment

--- week-6/1-use-memo/src/components/Assignment3.jsx
+++ week-6/1-use-memo/src/components/Assignment3.jsx
@@ -2,17 +2,24 @@ import React, { useState, useMemo } from 'react';
 // You have been given a list of items you shopped from the grocery store
 // You need to calculate the total amount of money you spent
 
-const Assignment3 = () => {
+export const Assignment3 = () => {
     const [items, setItems] = useState([
         { name: 'Chocolates', value: 10 },
         { name: 'Chips', value: 20 },
         { name: 'Onion', value: 30 },
         { name: 'Tomato', value: 30 },
+        { name: 'potato', value: 40}
         // Add more items as needed
     ]);
 
     // Your code starts here
-    const totalValue = 0;
+    const totalValue = useMemo(() => {
+        let totalValue = 0;
+        for (let i = 0; i < items.length; i++){
+            totalValue = totalValue + items[i].value
+        }
+        return totalValue
+    }, [items])
     // Your code ends here
     return (
         <div>
@@ -26,4 +33,3 @@ const Assignment3 = () => {
     );
 };
 
-export default Assignment3;
